2475408e80112d6cf8d4923683f6f528bbb74c5b
Andrew Mcgowan <c16470866@mydit.ie>
2020-12-27 23:25:04 +0000

    Removed Shooting Star

--- ProceduralSpace/Assets/ShootingStar.cs
+++ ProceduralSpace/Assets/ShootingStar.cs
@@ -7,6 +7,8 @@ public class ShootingStar : MonoBehaviour
     public float speed = 5.0f;
     private Transform target;
 
+    Color color;
+
     void Start()
     {
         //transform.position = new Vector3(0.0f, 0.0f, 0.0f);
@@ -14,6 +16,12 @@ public class ShootingStar : MonoBehaviour
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         sphere.transform.position = new Vector3(0f, 0f, 0f);
         sphere.transform.localScale = new Vector3(20f, 20f, 20f);
+
+       // Material mat = GetComponent<Material>();
+        Material mat = new Material(Shader.Find("New Material"));
+
+       // sphere.renderer.material = mat;
+
         
         //target = sphere.transform;
         //target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
