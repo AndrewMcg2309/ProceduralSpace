05f43daff31dfd9a3788851107a3b82b85c17eb8
Andrew Mcgowan <c16470866@mydit.ie>
2020-12-27 22:55:36 +0000

    Fixing star

---
+++ ProceduralSpace/Assets/MoveTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTransform : MonoBehaviour
+{
+    public GameObject sphere;
+    public Vector3 start_pos;
+    public Vector3 end_pos;
+    public float distance = 100f;
+
+    public float lerpTime;
+    public float currentLerpTime;
+
+
+
+    void Start()
+    {
+ 
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ ProceduralSpace/Assets/ShootingStar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingStar : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Transform target;
+
+    void Start()
+    {
+        //transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = new Vector3(0f, 0f, 0f);
+        sphere.transform.localScale = new Vector3(20f, 20f, 20f);
+        
+        //target = sphere.transform;
+        //target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
+        //target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
